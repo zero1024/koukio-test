433411d483cb7d748b8d473ff97b480e307bf5b5
oleg1024 <o.perekhod@profeelab.net>
2021-07-19 11:28:40 +0300

    add very basic implementation and test

--- src/main/java/com/koukio/Priorities.java
+++ src/main/java/com/koukio/Priorities.java
@@ -1,11 +1,33 @@
 package com.koukio;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.PriorityQueue;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Priorities {
 
     public static List<Student> getStudents(List<String> events) {
-        return null;
+
+        int numberOfEvents = Integer.parseInt(events.get(0));
+        var queue = new PriorityQueue<Student>(numberOfEvents);
+
+        events.stream().skip(1).forEach(event -> {
+            if (event.startsWith("ENTER")) {
+                String[] s = event.split(Pattern.quote(" "));
+                var student = new Student(Integer.parseInt(s[3]), s[1], Double.parseDouble(s[2]));
+                queue.add(student);
+                System.out.println("Add:" + student.getName());
+            } else {
+                var s = queue.poll();
+                System.out.println("Poll:" + s.getName());
+            }
+        });
+        
+        return Stream.generate(queue::poll)
+                .takeWhile(Objects::nonNull)
+                .toList();
     }
 
 }
--- src/test/java/com/koukio/PrioritiesTest.java
+++ src/test/java/com/koukio/PrioritiesTest.java
@@ -2,7 +2,8 @@ package com.koukio;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,6 +11,26 @@ public class PrioritiesTest {
 
     @Test
     void simpleTest() {
-        assertEquals(null, Priorities.getStudents(Collections.emptyList()));
+
+        var list = Arrays.asList(
+                "12",
+                "ENTER John 3.75 50",
+                "ENTER Mark 3.8 24",
+                "ENTER Shafaet 3.7 35",
+                "SERVED",
+                "SERVED",
+                "ENTER Samiha 3.85 36",
+                "SERVED",
+                "ENTER Ashley 3.9 42",
+                "ENTER Maria 3.6 46",
+                "ENTER Anik 3.95 49",
+                "ENTER Dan 3.95 50 ",
+                "SERVED"
+        );
+
+        List<String> res = Priorities.getStudents(list).stream().map(Student::getName).toList();
+        List<String> expectedRes = Arrays.asList("Dan", "Ashley", "Shafaet", "Maria");
+
+        assertEquals(expectedRes, res);
     }
 }
